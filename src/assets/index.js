b574f9924bb1d57f862711c38dcbd2dc661dec40
Saul Augusto Gasca Farrera <sgfarreradev@gmail.com>
2023-06-29 12:40:13 -0600

    3D Computer model added to the canvas for visualization add into Hero Component the main sectiin with main info

--- src/assets/index.js
+++ src/assets/index.js
@@ -6,6 +6,9 @@ import web from "./web.png";
 import github from "./github.png";
 import menu from "./menu.svg";
 import close from "./close.svg";
+import logocolor from "./logocolor.svg"
+import logoblack from "./logoblack.svg"
+import logowhite from "./logowhite.svg"
 
 import css from "./tech/css.png";
 import docker from "./tech/docker.png";
@@ -59,4 +62,6 @@ export {
   carrent,
   jobit,
   tripguide,
+  logoblack,
+  logowhite
 };
